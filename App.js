6f66b60c849a515534b5bf2c6448f6a96f749abf
Adnan Wahab <shel.bernstein123@gmail.com>
2024-02-26 13:44:26 -0600

    cool

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
 import Home from './screens/Home';
 import ColorPalette from './screens/ColorPalette';
@@ -19,64 +19,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  gridContainer: {
+    flex: 1,
+    margin: 2,
+  },
+  itemStyle: {
+    flex: 1,
+    margin: 5,
+    minWidth: 170,
+    maxWidth: 223,
+    height: 304,
+    maxHeight:304,
+    backgroundColor: '#CCC',
+  },
 });
 
-
-// const MainStack = createStackNavigator();
-// const RootStack = createStackNavigator();
-
-
-// 0. Speech + Action Tree for botparty 
-// 1. Hospital Administrator - see all patients, doctors, nurses, and medbots
-// 2. StarCraft Mode 
-// 3. Segmentation Map
-// 4. Call Of Duty Shopping -> AirDrop 
-// 5. Map w/ segmentation
-// 6. observable is organizing worlds information - FMA:B -> edward seeing the truth (like kapil gupta)
-// 7. Zed Vision -> whatever cool AI shader stuff you can think of lol
-// 8. Look up Foxglove
-// 9. Robotic Doctor Face + Voice 
-// 10. Karaoke Last
-
-// 11. Make App that takes a youtube Link and then parses it and turns it into a picture book
-// only need like 50% accuracy
-
-// const MainStackScreen = () => {
-//   return (
-//     <MainStack.Navigator>
-//       <MainStack.Screen name="Home" component={Home} />
-//       <MainStack.Screen
-//         name="ColorPalette"
-//         component={ColorPalette}
-//         options={({ route }) => ({ title: route.params.paletteName })}
-//       />
-//     </MainStack.Navigator>
-//   );
-// };
-
-// const ColorsAreCool = () => {
-//   return (
-//     <NavigationContainer>
-//       <RootStack.Navigator mode="modal">
-//         <RootStack.Screen
-//           name="Main"
-//           component={MainStackScreen}
-//           options={{ headerShown: false }}
-//         />
-//         <RootStack.Screen name="AddNewPalette" component={AddNewPaletteModal} />
-//       </RootStack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// import ComponentOne from './screens/ComponentOne';
-// import ComponentTwo from './screens/ComponentTwo';
-// import ComponentThree from './screens/ComponentThree';
-// import ComponentFour from './screens/ComponentFour';
-// import ComponentFive from './screens/ComponentFive';
-// import ComponentSix from './screens/ComponentSix';
-// import ComponentSeven from './screens/ComponentSeven';
-// import ComponentEight from './screens/ComponentEight';
 
 const ComponentOne = () => {
   return (<>
@@ -142,70 +99,35 @@ const ComponentEight = () => {
   </>)
 }
 
-// const NavigationButtons = () => {
-//   const navigation = useNavigation(); // Use the hook to get navigation object
-
-//   return (
-//     <>
-//     <Button title="Go to Component One" onPress={() => navigation.navigate('ComponentOne')} />
-// <Button title="Go to Component Two" onPress={() => navigation.navigate('ComponentTwo')} />
-// <Button title="Go to Component Three" onPress={() => navigation.navigate('ComponentThree')} />
-// <Button title="Go to Component Four" onPress={() => navigation.navigate('ComponentFour')} />
-// <Button title="Go to Component Five" onPress={() => navigation.navigate('ComponentFive')} />
-// <Button title="Go to Component Six" onPress={() => navigation.navigate('ComponentSix')} />
-// <Button title="Go to Component Seven" onPress={() => navigation.navigate('ComponentSeven')} />
-// <Button title="Go to Component Eight" onPress={() => navigation.navigate('ComponentEight')} />
-// </>
-//   )
-// }
-
-
-// const App = () => {
-
-
-
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello Swarmbotics AI</Text>
-//       <NavigationContainer>
-// <RootStack.Navigator mode="modal">
-//   <RootStack.Screen name="Main" component={MainStackScreen} options={{ headerShown: false }} />
-//   <RootStack.Screen name="AddNewPalette" component={AddNewPaletteModal} />
-//   {/* Define navigation to your components here */}
-//   <RootStack.Screen name="ComponentOne" component={ComponentOne} />
-//   <RootStack.Screen name="ComponentTwo" component={ComponentTwo} />
-//   <RootStack.Screen name="ComponentThree" component={ComponentThree} />
-//   <RootStack.Screen name="ComponentFour" component={ComponentFour} />
-//   <RootStack.Screen name="ComponentFive" component={ComponentFive} />
-//   <RootStack.Screen name="ComponentSix" component={ComponentSix} />
-//   <RootStack.Screen name="ComponentSeven" component={ComponentSeven} />
-//   <RootStack.Screen name="ComponentEight" component={ComponentEight} />
-// </RootStack.Navigator>
-// </NavigationContainer>
-// <NavigationButtons />
-//      </View>
-//   )
-// }
-
 const Stack = createStackNavigator();
 
 
 function HomeScreen({ navigation }) {
-  let numViews = 8;
   let numWords = 'one two three four five six seven eight'.split(' ')
 
-
   let content = numWords.map((word, index) => {
-    return (<Button
-      title={`Go to Screen ${index + 1}`}
-      onPress={() => navigation.navigate(word)}
-      />)
-  })
+    return {
+      key: `${index}`,
+      title: `Go to Screen ${index + 1}`,
+      navigateTo: word,
+    }
+  });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {content}
-    </View>
+    <FlatList
+      data={content}
+      renderItem={({ item }) => (
+        <View style={styles.itemStyle}>
+          <Button
+            title={item.title}
+            onPress={() => navigation.navigate(item.navigateTo)}
+          />
+        </View>
+      )}
+      numColumns={2}
+      keyExtractor={(item) => item.key}
+      contentContainerStyle={styles.gridContainer}
+    />
   );
 }
 
@@ -235,61 +157,4 @@ const App = () => {
   );
 }
 
-
-//main thread
-//bridge 
-//js - business logic? 
-
-
-
 export default App;
-
-
-// import { NavigationContainer } from '@react-navigation/native';
-// import { StyleSheet, Text, View } from 'react-native';
-
-
-
-
-// const App = () => {
-//   return (
-
-//     <NavigationContainer>
-//     <View style={styles.container}>
-//       <Text>Hello world</Text>
-//      </View>
-
-//      <Text>Hmm</Text>
-
-//       {/* Rest of the app code */}
-      
-//       </NavigationContainer>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-// import { StatusBar } from 'expo-status-bar';
-
-
-
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>{window.innerWidth}124</Text>
-//       <Text>Open up App.js to start working on your app!!!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
